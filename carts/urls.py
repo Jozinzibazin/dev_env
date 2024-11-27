11984d6d2b0415975039356ffa7f80228159a06f
Jozinzibazin <ivanrogov244@gmail.com>
2024-11-27 12:52:26 +0300

    FinalCommidIvan

--- carts/urls.py
+++ carts/urls.py
@@ -6,7 +6,7 @@ app_name = 'carts'# Устанавливаем имя приложения, чт
 
  # Список, который содержит все маршруты URL для данного приложения. Каждый маршрут сопоставляется с представлением
 urlpatterns = [
-    path('cart_add/<int:product_id>/', views.cart_add, name='cart_add'),
-    path('cart_change/<int:product_id>/', views.cart_change, name='cart_change'),  # URL '' будет соответствовать представлению catalog из файла views
-    path('cart_remove/<int:product_id>/', views.cart_remove, name='cart_remove'), #URL 'product/' будет соответствовать представлению product из файла views
+    path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
+    path('cart_change/<slug:product_slug>/', views.cart_change, name='cart_change'),  # URL '' будет соответствовать представлению catalog из файла views
+    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'), #URL 'product/' будет соответствовать представлению product из файла views
 ]
