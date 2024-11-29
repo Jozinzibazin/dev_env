66a1b6477ac6e9d7c632f32b10e6bf12b0c62a9c
seirg <seirgdeyl05@gmail.com>
2024-11-29 03:25:32 +0300

    JS add to cart

--- carts/urls.py
+++ carts/urls.py
@@ -6,7 +6,7 @@ app_name = 'carts'# Устанавливаем имя приложения, чт
 
  # Список, который содержит все маршруты URL для данного приложения. Каждый маршрут сопоставляется с представлением
 urlpatterns = [
-    path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
-    path('cart_change/<slug:product_slug>/', views.cart_change, name='cart_change'),  # URL '' будет соответствовать представлению catalog из файла views
-    path('cart_remove/<int:cart_id>/', views.cart_remove, name='cart_remove'), #URL 'product/' будет соответствовать представлению product из файла views
+    path('cart_add/', views.cart_add, name='cart_add'),
+    path('cart_change/', views.cart_change, name='cart_change'),  # URL '' будет соответствовать представлению catalog из файла views
+    path('cart_remove/', views.cart_remove, name='cart_remove'), #URL 'product/' будет соответствовать представлению product из файла views
 ]
