4573e988d986093bb695c89eb953ab7bc4253930
vasilij_rogov <vasilij_rogov@enot.work>
2024-12-11 22:26:41 +0300

    Revert "upd: imports"
    This reverts commit 3947db82bea236bb3c6702e75914f89108b6f0af.

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 from django.conf.global_settings import AUTH_USER_MODEL 
-import os 
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
