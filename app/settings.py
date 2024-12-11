8b24910335bb06f65dcc51028ee6eb1a99239a3d
vasilij_rogov <vasilij_rogov@enot.work>
2024-12-11 23:04:19 +0300

    fix

--- app/settings.py
+++ app/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 from django.conf.global_settings import AUTH_USER_MODEL 
-
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
